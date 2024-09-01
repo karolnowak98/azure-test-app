cfec0ed84b8175d9992efa5910d333f1abad0a4d
Karol Nowak <karol.nowak2298@gmail.com>
2024-09-01 17:54:17 +0200

    Init project (rider)

---
+++ azure-test-app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace azure_test_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
